560ae74fc4426cec38d9f02e97e8f3cfa1b72f1a
Edward Jimenez <edward.jimenez@britecore.com>
2023-04-27 22:10:03 -0500

    Backup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
+/**
+ * Root Application
+ * @return {JSX.Element}
+ * @constructor
+ */
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route index element={<DefaultApp />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<DefaultApp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-function DefaultApp(){
+/**
+ * Default App
+ * @return {JSX.Element}
+ * @constructor
+ */
+function DefaultApp() {
   return (
-      <div className="App">
-
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
